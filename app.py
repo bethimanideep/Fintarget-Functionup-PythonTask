8f54ae55d30b6613273e33f6a104bcd935a4e231
bethimanideep <bethimanideep@gmail.com>
2023-11-24 13:58:00 +0530

    implemented timer for 3 minutes moving averages

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
-
 import os
 import websocket
 import json
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 from statistics import mean
 
 WEBSOCKET_URL = "wss://functionup.fintarget.in/ws?id=fintarget-functionup"
 
-
 CSV_FILENAME = "olhc_data.csv"
 WINDOW_SIZE = 3
 
-additional_files = ["moving_averages_Finnifty.csv", "moving_averages_Banknifty.csv", "moving_averages_Nifty.csv",CSV_FILENAME]
+additional_files = ["moving_averages_Finnifty.csv", "moving_averages_Banknifty.csv", "moving_averages_Nifty.csv", CSV_FILENAME]
 
 for filename in additional_files:
     if os.path.exists(filename):
         os.remove(filename)
         print(f"Cleaned existing file: {filename}")
+
 olhc_data = {instrument: [] for instrument in ["Nifty", "Banknifty", "Finnifty"]}
 moving_averages_data = {}  # Use a separate dictionary for moving averages data
+start_time = datetime.now()
 
 def on_message(ws, message):
     data = json.loads(message)
     process_data(data)
 
 def process_data(data):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    global start_time
+    timestamp = datetime.now()
 
     for instrument, ltp in data.items():
         olhc_data[instrument].append({
@@ -49,30 +50,31 @@ def process_data(data):
             f.flush()
 
         if len(olhc_data[instrument]) >= WINDOW_SIZE:
-            close_prices = [entry["Close"] for entry in olhc_data[instrument][-WINDOW_SIZE:]]
-            ma = mean(close_prices)
+            # Check if at least three minutes have passed before calculating moving average
+            if (timestamp - start_time) >= timedelta(minutes=3):
+                close_prices = [entry["Close"] for entry in olhc_data[instrument][-WINDOW_SIZE:]]
+                ma = mean(close_prices)
 
-            # Add the new moving average value to the corresponding instrument's dictionary
-            moving_averages_data.setdefault(instrument, []).append({"Timestamp": timestamp, "MovingAverage": ma})
+                # Add the new moving average value to the corresponding instrument's dictionary
+                moving_averages_data.setdefault(instrument, []).append({"Timestamp": timestamp, "MovingAverage": ma})
 
-            # Save moving averages data to separate CSV file for each instrument
-            if instrument not in moving_averages_data:
-                continue
+                # Save moving averages data to a separate CSV file for each instrument
+                if instrument not in moving_averages_data:
+                    continue
 
-            df_ma = pd.DataFrame(moving_averages_data[instrument][-1:])  # Select only the last entry
-            filename = f"moving_averages_{instrument}.csv"
+                df_ma = pd.DataFrame(moving_averages_data[instrument][-1:])  # Select only the last entry
+                filename = f"moving_averages_{instrument}.csv"
 
-            with open(filename, 'a+', newline='') as f_ma:
-                # Write header only if the file is newly created
-                if f_ma.tell() == 0:
-                    df_ma.to_csv(f_ma, index=False)
-                else:
-                    df_ma.to_csv(f_ma, index=False, header=False)
+                with open(filename, 'a+', newline='') as f_ma:
+                    # Write header only if the file is newly created
+                    if f_ma.tell() == 0:
+                        df_ma.to_csv(f_ma, index=False)
+                    else:
+                        df_ma.to_csv(f_ma, index=False, header=False)
 
-                f_ma.flush()
+                    f_ma.flush()
 
-            print(f"Moving Average for {instrument}: {ma}")
-
+                print(f"Moving Average for {instrument}: {ma}")
 
 # Connect to WebSocket
 def connect_to_websocket():
